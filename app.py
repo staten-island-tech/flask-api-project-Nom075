b5800928d3e3eb4aeec1ca9b5242c97badccecf5
Nom075 <graytonl@nycstudents.net>
2025-04-28 10:16:57 -0400

    minecraft

--- app.py
+++ app.py
@@ -5,28 +5,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Get the list of countries from the API
-    response = requests.get("https://country-cities.herokuapp.com/api/v0.1/countries")
+    # Get the list of items from the new API
+    response = requests.get("https://raw.githubusercontent.com/anish-shanbhag/minecraft-api/refs/heads/master/data/items.json")
     
     if response.status_code == 200:
         data = response.json()
-        country_list = data['results']
         
-        # Create a list to hold country details
-        countries = []
+        # Create a list to hold item details
+        items = []
         
-        for country in country_list:
-            countries.append({
-                'name': country['name'],
-                'capital': country.get('capital', 'N/A'),
-                'population': country.get('population', 'N/A'),
-                'flag': country.get('flag', 'N/A')
+        for item in data:
+            items.append({
+                'name': item['name'],
+                'description': item.get('description', 'N/A'),
+                'image': item.get('image', 'N/A'),
+                'renewable': item.get('renewable', 'N/A'),
+                'stackSize': item.get('stackSize', 'N/A')
             })
         
-        # Send the list of countries to the index.html page
-        return render_template("index.html", countries=countries)
+        # Send the list of items to the index.html page
+        return render_template("index.html", items=items)
     else:
-        return "Failed to fetch country data", 500
+        return "Failed to fetch item data", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
